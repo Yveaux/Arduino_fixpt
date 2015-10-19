7d076a89ccada7abe1c510ee0d1bb38bf6b5b789
Daniel Zanco <dangpzanco@gmail.com>
2015-10-18 22:17:21 -0200

    ARM/AVR Compatibility

--- libfixmath_conf.h
+++ libfixmath_conf.h
@@ -1,15 +1,25 @@
 #ifndef _LIBFIXMATH_CONF_H
 #define _LIBFIXMATH_CONF_H
 
-#ifdef ARDUINO_ARCH_AVR
-// ARDUINO_ARCH_AVR might not be the optimum define to check on,
-// but nothing else is available...
+#if defined(__arm__) 
+
+//#define FIXMATH_NO_64BIT
+//#define FIXMATH_OPTIMIZE_8BIT
+//#define FIXMATH_NO_CACHE
+#define FIXMATH_NO_OVERFLOW
+//#define FIXMATH_NO_ROUNDING
+
+#elif defined(__AVR__) 
+
 // AVR Architecture is 8bit & limited in memory
 #define FIXMATH_NO_64BIT
 #define FIXMATH_OPTIMIZE_8BIT
 #define FIXMATH_NO_CACHE
-//#define FIXMATH_NO_OVERFLOW
+#define FIXMATH_NO_OVERFLOW
 //#define FIXMATH_NO_ROUNDING
+
+#else
+#error Architecture or board not supported.
 #endif
 
 #endif
